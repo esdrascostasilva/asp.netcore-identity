4128dd8b5a326b7fef954c861058dbf7cc039e30
esdrascostasilva <esdras.cts@outlook.com>
2021-04-23 08:36:19 -0300

    Improving the class Startup (wiping code).

---
+++ AspNetCoreIdentity/Config/IdentityConfig.cs
@@ -0,0 +1,26 @@
+﻿using AspNetCoreIdentity.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreIdentity.Config
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddAuthorizationConfig(this IServiceCollection services)
+        {
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("CanDelete", policy => policy.RequireClaim("CanDelete"));
+
+                options.AddPolicy("CanRead", policy => policy.Requirements.Add(new PermissionNecessary("CanRead")));
+                options.AddPolicy("CanWrite", policy => policy.Requirements.Add(new PermissionNecessary("CanWrite")));
+
+            });
+
+            return services;
+        }
+    }
+}
--- AspNetCoreIdentity/Startup.cs
+++ AspNetCoreIdentity/Startup.cs
@@ -1,19 +1,9 @@
-﻿using AspNetCoreIdentity.Areas.Identity.Data;
-using AspNetCoreIdentity.Extensions;
-using Microsoft.AspNetCore.Authorization;
+﻿using AspNetCoreIdentity.Config;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AspNetCoreIdentity
 {
@@ -26,39 +16,14 @@ namespace AspNetCoreIdentity
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-            services.AddDbContext<AspNetCoreIdentityContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("AspNetCoreIdentityContextConnection")));
-
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddRoles<IdentityRole>()
-                .AddDefaultUI(UIFramework.Bootstrap4)
-                .AddEntityFrameworkStores<AspNetCoreIdentityContext>();
-
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("CanDelete", policy => policy.RequireClaim("CanDelete"));
-
-                options.AddPolicy("CanRead", policy => policy.Requirements.Add(new PermissionNecessary("CanRead")));
-                options.AddPolicy("CanWrite", policy => policy.Requirements.Add(new PermissionNecessary("CanWrite")));
-
-            });
-
-            services.AddSingleton<IAuthorizationHandler, PermissionNecessaryHandler>();
-
+            services.AddIdentityConfig(Configuration);
+            services.AddAuthorizationConfig();
+            services.ResolveDependencies();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -76,9 +41,7 @@ namespace AspNetCoreIdentity
 
             app.UseMvc(routes =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute("default","{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
